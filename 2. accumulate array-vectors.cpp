6d0b1884a3b701f261e53d7199b7727027979daf
RANSHER RAJ <44178727+ransherraj@users.noreply.github.com>
2021-09-05 11:12:32 +0530

    Create 2. accumulate array-vectors.cpp

---
+++ 2. accumulate array-vectors.cpp
@@ -0,0 +1,12 @@
+//Leetcode problem 1672. Richest Customer Wealth
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int ans = 0;
+        for(int i = 0; i<accounts.size(); i++){
+            ans = max( ans, accumulate(accounts[i].begin(), accounts[i].end(), 0));
+        }
+        return ans;
+    }
+};
