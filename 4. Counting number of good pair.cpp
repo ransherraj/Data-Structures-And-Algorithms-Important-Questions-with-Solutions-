b9a79d8472a0bab1f9ff1893de33299c262f769f
RANSHER RAJ <44178727+ransherraj@users.noreply.github.com>
2021-09-05 17:17:23 +0530

    Create 4. Counting number of good pair.cpp

---
+++ 4. Counting number of good pair.cpp
@@ -0,0 +1,8 @@
+ int numIdenticalPairs(vector<int>& A) {
+        int res = 0;
+        unordered_map<int, int> count;
+        for (int a: A) {
+            res += count[a]++;
+        }
+        return res;
+    }
