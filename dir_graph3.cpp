ff2e397e33bba281668ea4f3913e16f0b269ab94
RANSHER RAJ <44178727+ransherraj@users.noreply.github.com>
2021-12-08 13:42:47 +0530

    Create dir_graph3.cpp

---
+++ dir_graph3.cpp
@@ -0,0 +1,178 @@
+
+// Directed Graphs in C++ Functions
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void createAdjList(vector<vector<int>> &adj, vector<vector<int>>edges )
+{
+    for(int i = 0; i<edges.size(); i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u].push_back(v);
+    }
+}
+
+void printGraph(vector<vector<int>> adj)
+{   
+    int V = adj.size();
+    for (int v = 0; v < V; ++v)
+    {
+        cout << "Adjacency list of vertex: "<< v << ": " ;
+        for (auto x : adj[v])
+           cout << "-> " << x;
+        cout<<endl;
+    }
+}
+
+/* DFS Traversal*/
+
+void dfsutil(vector<vector<int>> adj, vector<bool>&vis, int s){
+    
+    vis[s] = true;
+    cout<<s<<" ";
+    for(int x : adj[s]){
+        if(vis[x] == false){
+            dfsutil(adj, vis, x);
+        }
+    }
+}
+
+void DFS(vector<vector<int>> adj){
+    int V = adj.size();
+    vector<bool>vis(V, false);
+    // vector<int>res;
+    cout<<"DFS Traversal: ";
+    for(int i = 0; i<V; i++){
+        if(vis[i] == false){
+            dfsutil(adj, vis, i);
+        }
+    }
+}
+
+/* BFS Traversal*/
+
+void bfsutil(vector<vector<int>> adj, vector<bool>&vis, int s){
+    
+    vis[s] = true;
+    queue<int> q;
+    q.push(s);
+    while(!q.empty()){
+        int v = q.front();
+        cout<<v<<" ";
+        q.pop();
+        for(int x : adj[v]){
+            if(vis[x] == false){
+                vis[x] = true;
+                q.push(x);
+            }
+        }
+    }
+}
+
+void BFS(vector<vector<int>> adj){
+    int V = adj.size();
+    vector<bool>vis(V, false);
+    // vector<int>res;
+    cout<<"BFS Traversal: ";
+    for(int i = 0; i<V; i++){
+        if(vis[i] == false){
+            bfsutil(adj, vis, i);
+        }
+    }
+}
+
+
+/* Find shortest path*/
+
+void util_shortest(vector<vector<int>> adj, vector<bool>&vis, int s, vector<int> &dist ){
+    
+    vis[s] = true;
+    queue<int> q;
+    q.push(s);
+    while(!q.empty()){
+        int v = q.front();
+        q.pop();
+        for(int x : adj[v]){
+            if(vis[x] == false){
+                dist[x] = dist[v]+1;
+                vis[x] = true;
+                q.push(x);
+            }
+        }
+    }
+}
+
+void BFS_SHORTEST(vector<vector<int>> adj){
+    int V = adj.size();
+    vector<bool>vis(V, false);
+    vector<int>dist(V, 0);
+    
+    for(int i = 0; i<V; i++){
+        if(vis[i] == false){
+            util_shortest(adj, vis, i, dist);
+        }
+    }
+    cout<<endl<<"Shortest path from root node to other nodes::";
+    for(int i = 0; i<V; i++){
+        cout<<dist[i]<<" ";
+    }
+}
+
+
+
+/* Cycle detection */
+bool util_cycle(vector<vector<int>> adj, vector<bool>&vis, int s, vector<bool>&bac){
+
+    vis[s] = true;
+    bac[s] = true;
+    for(int x : adj[s]){
+        if(vis[x] == false){
+            if(util_cycle(adj, vis, x, bac) == true ) return true;
+        }
+        else if(bac[x] == true) return true;
+        
+    }
+    bac[s] = false;
+    return false;
+}
+
+bool DFS_CYCLE(vector<vector<int>> adj){
+    int V = adj.size();
+    vector<bool>vis(V, false);
+    vector<bool>bac(V, false);
+    for(int i = 0; i<V; i++){
+        if(vis[i] == false){
+            if(util_cycle(adj, vis, i, bac) == true) return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+int main()
+{
+    int V, edgeCount;
+    cin>>V>>edgeCount;
+    vector<vector<int>> edges;
+    vector<vector<int>> adj(V);
+    for(int i = 0; i<edgeCount; i++){
+        int a, b;
+        cin>>a>>b;
+        edges.push_back({a, b}); 
+    }
+    createAdjList(adj, edges);
+    printGraph(adj);
+    cout<<endl;
+    DFS(adj);
+    cout<<endl;
+    BFS(adj);
+    BFS_SHORTEST(adj);
+    cout<<endl<<"Cycle in graph: "<<DFS_CYCLE(adj)<<endl;
+    
+    cout<<"Topological Sorting : Kahn's Algorith:"<<endl;
+    
+    return 0;
+}
